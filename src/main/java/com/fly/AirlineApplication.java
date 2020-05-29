d06e36cf07fdb9a43b82d00832ddd5c12f9efbd1
Aleksandr Manivchuk <amanivhuk@gmail.com>
2020-05-29 17:34:46 +0300

    added corsConfigurer() in AirlineApplication.java and added field 'expiration' in TokenOutcomeDto.java

--- src/main/java/com/fly/AirlineApplication.java
+++ src/main/java/com/fly/AirlineApplication.java
@@ -2,11 +2,24 @@ package com.fly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AirlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AirlineApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
     }
 }
